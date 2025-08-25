630729a40471da153a9e8d0a63b0db676368f32b
Webpolka <webpolka@gmail.com>
2025-08-25 19:18:52 +0300

    new version 2.6

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ const siteOverlay = document.querySelector(".site-overlay");
 /*--------------------------------------------------------------------------------------------------------------
 CURRENCY BLOCK
 ----------------------------------------------------------------------------------------------------------------*/
-const currencyBlock = document.querySelector(".header-currency");
+const currencyBlock = document.querySelector(".header-top_currency");
 
 if (currencyBlock) {
 	currencyReturn();
@@ -19,17 +19,17 @@ if (currencyBlock) {
 		// Логика смены валюты и курса
 		// Например, переключение между EUR и USD
 		if (euroData && usdData) {
-			const currentCurrency = currencyBlock.querySelector(".header-currency_name").textContent;
+			const currentCurrency = currencyBlock.querySelector(".header-top_currency_name").textContent;
 			if (currentCurrency === "EUR") {
-				currencyBlock.querySelector(".header-currency_name").textContent = "USD";
-				currencyBlock.querySelector(".header-currency_rate").textContent = usdData; // обновите по актуальным данным
+				currencyBlock.querySelector(".header-top_currency_name").textContent = "USD";
+				currencyBlock.querySelector(".header-top_currency_rate").textContent = usdData; // обновите по актуальным данным
 			} else {
-				currencyBlock.querySelector(".header-currency_name").textContent = "EUR";
-				currencyBlock.querySelector(".header-currency_rate").textContent = euroData;
+				currencyBlock.querySelector(".header-top_currency_name").textContent = "EUR";
+				currencyBlock.querySelector(".header-top_currency_rate").textContent = euroData;
 			}
 		} else {
-			currencyBlock.querySelector(".header-currency_name").textContent = "none";
-			currencyBlock.querySelector(".header-currency_rate").textContent = "none"; // обновите по
+			currencyBlock.querySelector(".header-top_currency_name").textContent = "none";
+			currencyBlock.querySelector(".header-top_currency_rate").textContent = "none"; // обновите по
 		}
 	}
 }
@@ -142,88 +142,114 @@ const signSlideForm = document.querySelector(".authorize-signup");
 const loginSlideForm = document.querySelector(".authorize-login");
 
 const dataSignedBtn = document.querySelector("[data-signed]");
-const dataSignedText = dataSignedBtn.querySelector("span");
 
-const allHeaderSlideCloseBtns = [document.querySelector(".authorize-login_close"), document.querySelector(".authorize-signup_close")];
+const innerSignInSlideBtn = document.querySelector('[data-modal="sign-in"]');
+const innerSignUpSlideBtn = document.querySelector('[data-modal="sign-up"]');
 
-let timer;
-
-let signedBoolean;
 if (dataSignedBtn) {
-	signedBoolean = dataSignedBtn.dataset.signed === "true" ? true : false;
-	// Выставляем стейт кнопки
-	initSignButton(signedBoolean);
-}
+	const dataSignedText = dataSignedBtn.querySelector("span");
 
-function initSignButton(signedBoolean) {
-	if (signedBoolean && dataSignedText) {
-		dataSignedText.textContent = "Sign In";
-	} else if (!signedBoolean && dataSignedText) {
-		dataSignedText.textContent = "Sign Up";
-	}
-}
+	const allHeaderSlideCloseBtns = [document.querySelector(".authorize-login_close"), document.querySelector(".authorize-signup_close")];
 
-function removeNoScroll() {
-	const catOverlay = document.querySelector(".catsoverlay-wrapper");
+	let timer;
 
-	clearTimeout(timer);
-	timer = setTimeout(() => {
-		document.documentElement.classList.remove("no-scroll");
-		document.documentElement.style.paddingRight = "";
-		siteOverlay && siteOverlay.classList.remove("active");
-		if (catOverlay) {
-			catOverlay.style.paddingRight = "";
-		}
-	}, 0);
-}
-function addNoScroll() {
-	const catOverlay = document.querySelector(".catsoverlay-wrapper");
-	const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-	document.documentElement.classList.add("no-scroll");
+	innerSignInSlideBtn.addEventListener("click", () => {
+		signSlideForm.classList.remove("active");
+		removeNoScroll();
 
-	if (catOverlay) {
-		catOverlay.style.paddingRight = `${scrollbarWidth}px`;
-	}
-	document.documentElement.style.paddingRight = `${scrollbarWidth}px`;
-	siteOverlay && siteOverlay.classList.add("active");
-}
-
-// Прослушивание кнопки Sign Up или Sign In
-dataSignedBtn &&
-	dataSignedBtn.addEventListener("click", function (e) {
-		e.preventDefault();
-
-		if (signedBoolean && loginSlideForm) {
+		setTimeout(() => {
 			addNoScroll();
 			loginSlideForm.classList.add("active");
-			console.log("Пользователь уже зарегистрирован !");
-		} else if (!signedBoolean && signSlideForm) {
-			addNoScroll();
-			signSlideForm.classList.add("active");
-			console.log("Пользователю нужно зарегистрироваться !");
-		}
-		return;
+		}, 300);
 	});
 
-// Прослушивание всех кнопок закрыть для слайд форм авторизации
-allHeaderSlideCloseBtns.forEach((btn) => {
-	btn.addEventListener("click", function (e) {
-		e.preventDefault();
-		signSlideForm.classList.remove("active");
+	innerSignUpSlideBtn.addEventListener("click", () => {
 		loginSlideForm.classList.remove("active");
 		removeNoScroll();
-	});
-});
 
-// Очищаем все поля паролей при нажатии reset password
-document.querySelectorAll(".panel-reset-btn").forEach((btn) => {
-	btn.addEventListener("click", function () {
-		document.querySelectorAll("input[type='password']").forEach((input) => {
-			input.value = "";
-			input.focus();
+		setTimeout(() => {
+			addNoScroll();
+			signSlideForm.classList.add("active");
+		}, 300);
+	});
+
+	let signedBoolean;
+	if (dataSignedBtn) {
+		signedBoolean = dataSignedBtn.dataset.signed === "true" ? true : false;
+		// Выставляем стейт кнопки
+		initSignButton(signedBoolean);
+	}
+
+	function initSignButton(signedBoolean) {
+		if (signedBoolean && dataSignedText) {
+			dataSignedText.textContent = "Sign In";
+		} else if (!signedBoolean && dataSignedText) {
+			dataSignedText.textContent = "Sign Up";
+		}
+	}
+
+	function removeNoScroll() {
+		const catOverlay = document.querySelector(".catsoverlay-wrapper");
+
+		clearTimeout(timer);
+		timer = setTimeout(() => {
+			document.documentElement.classList.remove("no-scroll");
+			document.documentElement.style.paddingRight = "";
+			siteOverlay && siteOverlay.classList.remove("active");
+			if (catOverlay) {
+				catOverlay.style.paddingRight = "";
+			}
+		}, 0);
+	}
+	function addNoScroll() {
+		const catOverlay = document.querySelector(".catsoverlay-wrapper");
+		const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+		document.documentElement.classList.add("no-scroll");
+
+		if (catOverlay) {
+			catOverlay.style.paddingRight = `${scrollbarWidth}px`;
+		}
+		document.documentElement.style.paddingRight = `${scrollbarWidth}px`;
+		siteOverlay && siteOverlay.classList.add("active");
+	}
+
+	// Прослушивание кнопки Sign Up или Sign In
+	dataSignedBtn &&
+		dataSignedBtn.addEventListener("click", function (e) {
+			e.preventDefault();
+
+			if (signedBoolean && loginSlideForm) {
+				addNoScroll();
+				loginSlideForm.classList.add("active");
+				console.log("Пользователь уже зарегистрирован !");
+			} else if (!signedBoolean && signSlideForm) {
+				addNoScroll();
+				signSlideForm.classList.add("active");
+				console.log("Пользователю нужно зарегистрироваться !");
+			}
+			return;
+		});
+
+	// Прослушивание всех кнопок закрыть для слайд форм авторизации
+	allHeaderSlideCloseBtns.forEach((btn) => {
+		btn.addEventListener("click", function (e) {
+			e.preventDefault();
+			signSlideForm.classList.remove("active");
+			loginSlideForm.classList.remove("active");
+			removeNoScroll();
 		});
 	});
-});
+
+	// Очищаем все поля паролей при нажатии reset password
+	document.querySelectorAll(".panel-reset-btn").forEach((btn) => {
+		btn.addEventListener("click", function () {
+			document.querySelectorAll("input[type='password']").forEach((input) => {
+				input.value = "";
+				input.focus();
+			});
+		});
+	});
+}
 
 /*--------------------------------------------------------------------------------------------------------------
 DATA-TABS LISTENER
@@ -231,16 +257,18 @@ DATA-TABS LISTENER
 import Tabs from "./modules/tabs";
 const authorizeLoginElement = document.querySelector(".authorize-login");
 const authorizeSignUpElement = document.querySelector(".authorize-signup");
+const authorizeBtn = document.querySelector("[data-signed]");
 
-authorizeLoginElement &&
-	new Tabs({
-		parent: ".authorize-login",
-	});
-authorizeSignUpElement &&
-	new Tabs({
-		parent: ".authorize-signup",
-	});
-
+if (authorizeBtn) {
+	authorizeLoginElement &&
+		new Tabs({
+			parent: ".authorize-login",
+		});
+	authorizeSignUpElement &&
+		new Tabs({
+			parent: ".authorize-signup",
+		});
+}
 /*--------------------------------------------------------------------------------------------------------------
 DATA-CATEGORIES LISTENER
 ----------------------------------------------------------------------------------------------------------------*/
@@ -349,31 +377,65 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 /*--------------------------------------------------------------------------------------------------------------
-MODAL WINDOWS
+PROFILE BTN
 ----------------------------------------------------------------------------------------------------------------*/
-import ModalAuth from "./modules/modal-auth";
-// Используем для каждого модального окна, передавая его DOM-элемент
+
+const profileBtn = document.querySelector("#profile-btn");
+const profileDropdown = document.querySelector(".header-profile_dropdown");
+
+if (profileBtn && profileDropdown) {
+	handleEvent();
+	function handleEvent() {
+		const rectLeft = profileBtn.offsetLeft;
+		const rectWidth = profileBtn.offsetWidth;
+
+		profileDropdown.style.display = "block";
+		const profileDropdownWidth = profileDropdown.offsetWidth;
+		profileDropdown.style.removeProperty("display");
+
+		profileDropdown.style.left = `${rectLeft - profileDropdownWidth + rectWidth}px`;
+	}
+
+	// Добавляем обработчики событий для window
+	window.addEventListener("resize", handleEvent);
+	window.addEventListener("change", handleEvent);
+
+	profileBtn.addEventListener("click", () => {
+		profileDropdown.classList.toggle("show");
+	});
+}
+
+
+/*--------------------------------------------------------------------------------------------------------------
+LISTS BUTTONS-PANELS LISTENER
+----------------------------------------------------------------------------------------------------------------*/
+
 document.addEventListener("DOMContentLoaded", () => {
-	const signInModal = document.querySelector('[data-signInModal="true"]');	
-	signInModal &&
-		new ModalAuth(signInModal, {
-			selectors: {
-				openBtn: '[data-modal="sign-in"]',
-				closeBtn: '[data-signInModal="close-btn"]',
-				backBtn: '[data-signInModal="back-btn"]',
-				continueBtn: '[data-signInModal="continue"]',
-				submitBtn: 'button[type="submit"]',
-				greatName: ".greating",
-				firstPart: ".modal-sign-in_first",
-				secondPart: ".modal-sign-in_second",
-				form: "form",
-				firstInput: "[data-signInModal='nikname']",
-				secondInput: "[data-signInModal='password']",
-				resetFirstBtn: '[data-signInModal="reset-nikname"]',
-				resetSecondBtn: '[data-signInModal="reset-password"]',
-				authorizeLogin: ".authorize-login",
-				authorizeSignUp: ".authorize-signup",
-				siteOverlay: ".site-overlay",
-			},
-		});	
+	const allListsButtons = document.querySelectorAll("[data-listBtn]");
+
+	allListsButtons.forEach((btn) => {
+	
+		btn.addEventListener("click", () => {
+			btn.classList.toggle("active");
+			
+			const id = btn.dataset.listbtn;
+			const dropdown = document.querySelector(`[data-listPanel="${id}"]`);
+
+			if (dropdown) {
+				dropdown.classList.toggle("active");
+
+				if (dropdown.classList.contains("active")) {
+					dropdown.style.maxHeight = dropdown.scrollHeight + 15 + "px";
+					dropdown.style.paddingBottom = 15 + "px";
+					dropdown.style.marginTop = -5 + "px";
+				} else if (!dropdown.classList.contains("active")) {
+					dropdown.style.removeProperty("max-height");
+					dropdown.style.removeProperty("padding-bottom");
+					dropdown.style.removeProperty("margin-top");
+				}
+			} else {
+				console.log(`панель data-listPanel="${id}" не найдена`);
+			}
+		});
+	});
 });
